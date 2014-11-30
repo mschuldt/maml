aa7c73e0a43e09bd6dc5646c57ac43e51ef9c5c6
mschuldt <mbschuldt@gmail.com>
2014-11-30 00:23:23 -0800

    separate file for serial

--- maml.py
+++ maml.py
@@ -3,7 +3,7 @@
 from maml_compile import compile
 from operator import add
 from sys import argv
-
+from maml_serial import Maml_serial
 
 #maps block names to bytecode arrays and
 #function names to FunctionByteCode objects
@@ -138,26 +138,7 @@ class Arduino:
         for c in compile(code):
             #TODO: check for errors
             self.serial.send_byte(c)
-        
-    
-class Maml_serial:
-    "Automatically find and maintain a connection to an Arduino over serial"
-    def __init__(self, speed=9600, port=None):
-        pass
 
-    def send_bytecode(self, code):
-        "send a byte over serial"
-        #TODO: how to handle disconnection and other errors?
-        if type(code) is int:
-            self.send_byte(code)
-        elif type(code) is str:
-            self.send_str(code)
-        else:
-            print("ERROR (send_bytecode): invalid bytecode type") #TODO
-    def send_byte(byte):
-        pass
-    def send_str(string):
-        pass
         
 if __name__ == '__main__':
     if len(argv) != 2:
