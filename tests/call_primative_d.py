b0c8ca17786d0d6f8a4943ae1857825355a1f551
mschuldt <mbschuldt@gmail.com>
2014-12-06 22:47:47 -0800

    test all possible parameter numbers

--- tests/call_primative_d.py
+++ tests/call_primative_d.py
@@ -5,6 +5,11 @@ def test():
     print_i(5)
     print_i(5 + 6)
     print_i(1 + 2 + 3)
+    test_add2(1, 2)
+    test_add3(1+1, 2, 3)
+    test_add4(1+1, 2, 3, 4)
+    test_add5(1+1, 2, 3, 4, 5)
+    test_add6(1+1, 2, 3, 4, 5, 6)
     quit(0)
     
 arduino.send(test)
