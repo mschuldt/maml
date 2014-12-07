dee82ee83969c58a7bf38d0a671612d4853ab5ec
mschuldt <mbschuldt@gmail.com>
2014-12-06 22:47:06 -0800

    primitives used for testing

--- primitives.c
+++ primitives.c
@@ -5,10 +5,6 @@ void delay(int seconds);
 _PRIMITIVE_
 int print_i(int n){
   printf("==> %d\n", n);
-
-_PRIMITIVE_
-void test_add(int a, int b){
-  print_i(a + b);
 }
 
 _PRIMITIVE_
@@ -26,3 +22,27 @@ int quit(int code){
   printf("bye.\n");
   exit(code);
 }
+
+///primitives used for testing
+#if !arduino
+_PRIMITIVE_
+void test_add2(int a, int b){
+  print_i(a + b);
+}
+_PRIMITIVE_
+void test_add3(int a, int b, int c){
+  print_i(a + b + c);
+}
+_PRIMITIVE_
+void test_add4(int a, int b, int c, int d){
+  print_i(a + b + c + d);
+}
+_PRIMITIVE_
+void test_add5(int a, int b, int c, int d, int e){
+  print_i(a + b + c + d + e);
+}
+_PRIMITIVE_
+void test_add6(int a, int b, int c, int d, int e, int f){
+  print_i(a + b + c + d + e + f);
+}
+#endif
