f5dcbb3999c6dfe7f3f629d15c3a1564dc98b9aa
mschuldt <mbschuldt@gmail.com>
2014-12-06 22:40:10 -0800

    'quit' and 'delay' primitives

--- primitives.c
+++ primitives.c
@@ -1,12 +1,28 @@
 #define _PRIMITIVE_
 
+void delay(int seconds);
+
 _PRIMITIVE_
-int print_int(int n){
+int print_i(int n){
   printf("==> %d\n", n);
-  sleep(1);
-}
 
 _PRIMITIVE_
 void test_add(int a, int b){
-  print_int(a + b);
+  print_i(a + b);
+}
+
+_PRIMITIVE_
+void delay(int ms){
+#if arduino
+  delay(ms);
+#else
+  sleep(ms); //TODO: delay for milliseconds instead of seconds
+#endif
+}
+
+//TODO: way of defining function from standard lib as primitives
+_PRIMITIVE_
+int quit(int code){
+  printf("bye.\n");
+  exit(code);
 }
