34c30b357fdf70d3a19e206867dae6d2c8e8d6f9
mschuldt <mbschuldt@gmail.com>
2014-12-09 12:47:12 -0800

    separate file for non-arduino primitives

--- process_primitives.el
+++ process_primitives.el
@@ -6,6 +6,7 @@
 ;;output: _prim.c, _prim.py
 
 (setq primitives_files '("primitives.c" "avm.c")
+      non_arduino_files '("non_arduino_primitives.c")
       c_out "_prim.c"
       py_out "_prim.py"
       token "_PRIMITIVE_"
@@ -13,43 +14,56 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq names nil)
-
-(defun process_primitives ()
-  (dolist (file primitives_files)
+(defun process_primitives (file)
+  (let (names)
     (find-file file)
     (while (re-search-forward  re nil :noerror)
-      (setq names (cons (match-string 2) names)))))
+      (setq names (cons (match-string 2) names)))
+    names))
 
-(defun write_py ()
-  (find-file py_out)
-  (erase-buffer)
-  (insert "primitives = {}\n")
-  (let ((c 0)
-        (p names))
-    (while p
-      (insert (format "primitives['%s'] = %s\n" (car p) c))
+(defun write_py (names &optional start indent)
+  (let ((c (or start 0)))
+    (while names
+      (insert (format "primitives['%s'] = %s\n" (car names) c))
       (setq c (1+ c)
-            p (cdr p))))
-  (save-buffer))
+            names (cdr names)))))
 
-
-(defun write_c ()
-  (find-file c_out)
-  (erase-buffer)
-  (let ((c 0)
-        (p names)
-        (l (length names)))
-    (insert (format "primitives = (void**)malloc(sizeof(void*)*%s);\n"
-                    (length names)))
-    (insert (format "n_primitives = %s;\n" l))
-    (while p
-      (insert (format "primitives[%s] = (void*)&%s;\n" c (car p)))
+(defun write_c (names &optional start)
+  (let ((c (or start 0)))
+    (while names
+      (insert (format "primitives[%s] = (void*)&%s;\n" c (car names)))
       (setq c (1+ c)
-            p (cdr p))))
-  (save-buffer))
+            names (cdr names)))))
 
-(process_primitives)
-(write_py)
-(write_c)
+(setq names nil
+      na_names nil)
+(dolist (file primitives_files)
+  (setq names (append (process_primitives file) names)))
+(dolist (file non_arduino_files)
+  (setq na_names (append (process_primitives file) na_names)))
+(setq l_names (length names)
+      l_na_names (length na_names))
 
+(find-file py_out)
+(erase-buffer)
+(insert "primitives = {}\n")
+(write_py names)
+;;TODO: need some variable that is set when not compiling for arduino
+(write_py na_names l_names t)
+(save-buffer)
+
+
+(find-file c_out)
+(erase-buffer)
+(insert (format "#if arduino
+n_primitives = %s;
+#else
+n_primitives = %s;
+#endif
+primitives = (void**)malloc(sizeof(void*)*n_primitives);
+" l_names (+ l_names l_na_names)))
+(write_c names)
+(insert "#if ! arduino\n")
+(write_c na_names l_names)
+(insert "#endif\n")
+(save-buffer)
