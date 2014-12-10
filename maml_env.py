95548de3c85c736c2e7110577f12d5ad668e013f
ihgann <ian.gann@berkeley.edu>
2014-12-09 22:47:33 -0800

    Some formatting

--- maml_env.py
+++ maml_env.py
@@ -7,13 +7,16 @@ class env:
         self.types = build_int_types
         self.n_names = 0
         self.label_counter = -1
-        self.funcTypes = {}
-        #TODO: track the size of the variable arrays in the Arduino
+        self._funcTypes = {}
+        # TODO: track the size of the variable arrays in the Arduino
 
     def get_store_index(self, name):
-        """returns the index at which to store NAME
-        return format: (global_p, index)"""
-        #check if this variable was declared 'global'
+        """
+        Returns the index at which to store NAME
+        return format: (global_p, index)
+        """
+
+        # check if this variable was declared 'global'
         if name in self.global_names:
             return self.parent.get_store_index(name)
 
@@ -22,20 +25,24 @@ class env:
         if name in self.names:
             return (globalp, self.names[name])
         else:
-            #TODO: check if index is greater then globals variable array on
-            #      arduino, send signal/codes to grow it if needed
-            #print("declared '{}'".format(name))
+            # TODO: Check if index is greater then globals variable array on
+            #       arduino, send signal/codes to grow it if needed
+            # print("declared '{}'".format(name))
             index = self.n_names
             self.names[name] = index
             self.n_names += 1
             return (globalp, index)
 
     def get_load_index(self, name):
-        """returns teh index ast which to load NAME.
-        return format: (global_p, index)
-        for NAME to have a load index, it must have been stored (declared) first
-        (the corresponding call to get_store_index must have been made)"""
-        #check if this variable was declared 'global'
+        """
+        Returns the index ast which to load NAME.
+        Return format: (global_p, index)
+        For NAME to have a load index, it must have been stored (declared)
+        first (the corresponding call to get_store_index must have been
+        made)
+        """
+
+        # Check if this variable was declared 'global'
         if name in self.global_names:
             return self.parent.get_load_index(name)
 
@@ -44,45 +51,59 @@ class env:
         if name in self.names:
             return (globalp, self.names[name])
 
-        #else: if we are in local scope, check global scope for index, else error
+        # Else: if we are in local scope, check global scope for index, else
+        # error
         if self.parent:
             return self.parent.get_load_index(name)
-        #else: no index found
+        # Else: no index found
         print("Error: name '{}' is not defined".format(name))
-        exit(1) #TODO: just terminate compilation, not the whole program
+        exit(1)  # TODO: just terminate compilation, not the whole program
 
     def declare_global(self, name):
         assert self.parent, "cannot add globals in global scope"
         self.global_names.add(name)
 
     def make_label(self):
-        "returns a unique label marker"
+        """
+        Returns a unique label marker
+        """
+
         self.label_counter += 1
         return self.label_counter
 
-    def declare_type(self, name, typ):
-        "declare NAME to have static type TYP"
-        #this currently allows for redeclaring types
-        self.types[name] = typ;
+    def declare_type(self, name, _type):
+        """
+        Declare NAME to have static type TYPE
+        """
+
+        # This currently allows for redeclaring types
+        self.types[name] = _type
 
     def get_type(self, name):
-        "returns the type of varaible NAME"
+        """
+        Returns the type of varaible NAME
+        """
+
         if self.parent and name in self.global_names:
             return self.parent.get_type(name)
-        typ = self.types.get(name, None)
-        if typ: return typ
-        #TODO: pass ast node so that line/col numbers can be printed
+        _type = self.types.get(name, None)
+        if _type:
+            return _type
+        # TODO: pass ast node so that line/col numbers can be printed
         print("Error: name '{}' is not declared".format(name))
         exit(1)
 
     def is_declared(self, name):
-        "check if NAME is declared"
+        """
+        Check if NAME is declared
+        """
+
         if self.parent and name in self.global_names:
             return self.parent.is_declared(name)
         return name in self.types
 
-    def createFuncTypes(funcName, argTypes, returnType):
-        self.funcTypes[funcName] = funcTypes(argTypes, returnType)
+    def createFuncTypes(self, funcName, argTypes, returnType):
+        self._funcTypes[funcName] = self.funcTypes(argTypes, returnType)
 
     class funcTypes:
 
@@ -91,6 +112,6 @@ class env:
             self.returnType = returnType
 
 
-build_int_types = {'true' : 'int',
-                   'false' : 'int',
+build_int_types = {'true': 'int',
+                   'false': 'int',
                    'none': 'int'}
