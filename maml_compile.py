79d92a86fe707d6793fc7157be1ca15400ef8ad8
mschuldt <mbschuldt@gmail.com>
2014-12-09 18:53:21 -0800

    type declarations

--- maml_compile.py
+++ maml_compile.py
@@ -140,6 +140,18 @@ def _(ast, env):
     ast['s_type'] = '(' + prev_type + ')'
 
 ################################################################################
+# declaration
+
+@code_gen('declaration')
+def _(ast, btc, env, top):
+    pass
+
+@type_check('declaration')
+def _(ast, env):
+    #TODO:
+    env.declare_type(ast['name'], ast['s_type'])
+
+################################################################################
 # name
 
 #TODO: 'name' and 'assign' still need to be tested with local names
@@ -202,7 +214,7 @@ def _(ast, env):
         print("Error: cannot assign variable of type {} to type {}"
                       .format(ast[targets]['s_type'], ast[value]['s_type']))
         exit(1)
-    
+
 
 ################################################################################
 # expr
